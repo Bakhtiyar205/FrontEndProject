2b6dd12f4f23e77e185d41795f8e6600888ee5e4
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-02-13 13:38:48 +0400

    partial home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function(){
-    
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 })

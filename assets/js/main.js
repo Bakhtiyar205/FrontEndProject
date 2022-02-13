9babfcec20cee359141744ea5dda645b838ebfc0
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-02-14 03:56:00 +0400

    yellow-card

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,51 @@
-$(document).ready(function(){
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:1
-            },
-            1000:{
-                items:1
-            }
-        }
-    })
+$(document).ready(function () {
+  $(".owl-carusel-store").owlCarousel({
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
+  $(".owl-carousel-cream").owlCarousel({
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
 
-    var clock = $('.clock').FlipClock({
-        clockFace: 'DailyCounter',
-        autoStart: false,
-        callbacks: {
-          stop: function() {
-            $('.message').html('The clock has stopped!')
-          }
-        }
-    });
+  var clock = $(".clock").FlipClock({
+    clockFace: "DailyCounter",
+    autoStart: false,
+    callbacks: {
+      stop: function () {
+        $(".message").html("The clock has stopped!");
+      },
+    },
+  });
 
-    // set time
-clock.setTime(220880);
+  // set time
+  clock.setTime(220880);
 
-// countdown mode
-clock.setCountdown(true);
+  // countdown mode
+  clock.setCountdown(true);
 
-// start the clock
-clock.start();
-})
+  // start the clock
+  clock.start();
+});

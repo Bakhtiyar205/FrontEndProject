ba1d4b04eaa11cd3d1472cf4678ddbdea14b0e16
Bakhtiyar Amrahov <bakhtiyaria@code.edu.az>
2022-02-13 18:30:44 +0400

    clock

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,4 +14,23 @@ $(document).ready(function(){
             }
         }
     })
+
+    var clock = $('.clock').FlipClock({
+        clockFace: 'DailyCounter',
+        autoStart: false,
+        callbacks: {
+          stop: function() {
+            $('.message').html('The clock has stopped!')
+          }
+        }
+    });
+
+    // set time
+clock.setTime(220880);
+
+// countdown mode
+clock.setCountdown(true);
+
+// start the clock
+clock.start();
 })
